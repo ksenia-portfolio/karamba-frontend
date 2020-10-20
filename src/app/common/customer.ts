974cc773b92ec49049f8a1c6add7b6cf9c5f8bf0
kunica777 <kunica777@gmail.com>
2020-10-20 13:38:55 +0300

    v1

--- src/app/common/customer.ts
+++ src/app/common/customer.ts
@@ -1,2 +1,8 @@
 export class Customer {
+
+  id: bigint;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Customer} from '../common/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private baseUrl = 'http://localhost:8080/customers';
+  private purchaseUrl = 'http://localhost:8080/purchase';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  // find customer by id
+  getCustomerById(customerId: number): Observable<Customer> {
+    const searchUrl = `${this.baseUrl}/${customerId}`;
+    return this.httpClient.get<Customer>(searchUrl);
+  }
+
+  // find customer by email
+  getCustomerByEmail(customerEmail: string): Observable<Customer> {
+    const searchUrl = `/search/findCustomerByEmail?email=${customerEmail}`;
+    return this.httpClient.get<Customer>(searchUrl);
+  }
+
 }
+
